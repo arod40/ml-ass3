ba8b97a5118a4fc271c4c39c8640765d5a3c5f17
arod40 <arodacademic@gmail.com>
2022-03-14 14:15:02 -0500

    implement numpy logistic regression

--- ex1.py
+++ ex1.py
@@ -4,6 +4,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def gradient_ascent(target, d, bounds, max_steps=10000, lr=0.1):
+    lower, upper = bounds
+
+    best_f = inf
+    best_w = None
+    f_prev = inf
+
+    eps = 1e-8
+
+    w = np.zeros((d, 1))
+
+    it = 0
+    while it < max_steps:
+        it += 1
+
+        f, gradient = target(w)
+        w += lr * gradient
+
+        if not (w >= lower).all() and (w <= upper).all():
+            continue
+
+        if abs(f_prev - f) < eps:
+            break
+        else:
+            f_prev = f
+
+        if f < best_f:
+            best_w = w
+            best_f = f
+
+    return best_w, best_f, it, lr
+
+
 def pocket_algorithm(X, y, max_iter=-1, w_init=None):
     def perceptron(x, w):
         val = x @ w
@@ -34,6 +67,23 @@ def pocket_algorithm(X, y, max_iter=-1, w_init=None):
 def linear_regression(X, y):
     X_t = X.transpose()
     return np.linalg.inv(X_t @ X) @ X_t @ y
+
+
+def logistic_regression(X, y, max_iter, lr):
+    sigmoid = np.vectorize(lambda x: 1 / (1 + np.e ** (-x)))
+    X_t = X.transpose()
+
+    def ce(w):
+        N = X.shape[0]
+        f = np.log(X @ w).sum() / N
+        gradient = X_t @ (y * sigmoid(-y * (X @ w)))
+
+        return f, gradient
+
+    w, _, _, _, = gradient_ascent(
+        ce, X.shape[1], (-inf, inf), max_steps=max_iter, lr=lr
+    )
+    return w
 
 
 def plot_line(w, x1, x2, color="black", label=""):
@@ -85,27 +135,40 @@ if __name__ == "__main__":
     w_lg = linear_regression(X, y)
     w_p, inc1 = pocket_algorithm(X, y, max_iter=10000)
     w_p2, inc2 = pocket_algorithm(X, y, max_iter=10000, w_init=w_lg)
+    w_log = logistic_regression(X, y, max_iter=10000, lr=0.1)
 
-    plot_line(list(w_lg), X[:, 1].min(), X[:, 1].max(), "blue")
-    plot_line(list(w_p), X[:, 1].min(), X[:, 1].max(), "red")
-    plot_line(list(w_p2), X[:, 1].min(), X[:, 1].max(), "green")
+    plot_line(
+        list(w_lg), X[:, 1].min(), X[:, 1].max(), "blue", label="linear regression"
+    )
+    plot_line(list(w_p), X[:, 1].min(), X[:, 1].max(), "red", label="pocket algorithm")
+    # plot_line(
+    #     list(w_p2),
+    #     X[:, 1].min(),
+    #     X[:, 1].max(),
+    #     "green",
+    #     label="pocket with lin regression init",
+    # )
+    plot_line(
+        list(w_log), X[:, 1].min(), X[:, 1].max(), "black", label="logistic regression"
+    )
+    plt.legend()
     plt.show()
 
     plt.xlim(X[:, 1].min() - 1, X[:, 1].max() + 1)
     plt.ylim(X[:, 2].min() - 1, X[:, 2].max() + 1)
 
-    y_outliers = add_outliers(y)
-    y_ = y_outliers.squeeze()
-    plt.scatter(X[y_ == 1, 1], X[y_ == 1, 2], marker="o", color="blue")
-    plt.scatter(X[y_ == -1, 1], X[y_ == -1, 2], marker="x", color="red")
+    # y_outliers = add_outliers(y)
+    # y_ = y_outliers.squeeze()
+    # plt.scatter(X[y_ == 1, 1], X[y_ == 1, 2], marker="o", color="blue")
+    # plt.scatter(X[y_ == -1, 1], X[y_ == -1, 2], marker="x", color="red")
 
-    y = y_outliers
-    w_lg = linear_regression(X, y)
-    w_p, inc1 = pocket_algorithm(X, y, max_iter=10000)
-    w_p2, inc2 = pocket_algorithm(X, y, max_iter=10000, w_init=w_lg)
+    # y = y_outliers
+    # w_lg = linear_regression(X, y)
+    # w_p, inc1 = pocket_algorithm(X, y, max_iter=10000)
+    # w_p2, inc2 = pocket_algorithm(X, y, max_iter=10000, w_init=w_lg)
 
-    plot_line(list(w_lg), X[:, 1].min(), X[:, 1].max(), "blue")
-    plot_line(list(w_p), X[:, 1].min(), X[:, 1].max(), "red")
-    plot_line(list(w_p2), X[:, 1].min(), X[:, 1].max(), "green")
-    plt.show()
+    # plot_line(list(w_lg), X[:, 1].min(), X[:, 1].max(), "blue")
+    # plot_line(list(w_p), X[:, 1].min(), X[:, 1].max(), "red")
+    # plot_line(list(w_p2), X[:, 1].min(), X[:, 1].max(), "green")
+    # plt.show()
 
